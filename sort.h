875f144b43405769e3645080a01ebb090e48cfc8
Sadie <saderrs822@gmail.com>
2015-02-23 23:29:12 -0600

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,12 @@
+//Sadie Jungers
+//Sorting Functions Header
+#include <iostream>
+
+void shell(int[],int,int);
+void bubble(int[],int,int);
+void selection(int[],int,int);
+void quick(int[],int,int);
+void quicki(int[],int,int);
+void partition(int[],int,int,int&);
+void swap(int&,int&);
+void heap(int[],int,int);
